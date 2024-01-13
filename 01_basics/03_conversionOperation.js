7b5b1f5c492a9addb97a8a9d73ca693673a8bf89
AmitSharma1201 <155089920+AmitSharma1201@users.noreply.github.com>
2024-01-13 10:12:34 +0000

    comversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,6 +14,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = Strin(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//***************OPERATIONS**************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1+str2;
+console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(true);
+//console.log(+true);
+//console.log(1+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
