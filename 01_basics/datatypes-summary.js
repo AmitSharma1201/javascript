294802e9789bffcd38fd9f08af15eb525bd9f843
AmitSharma1201 <155089920+AmitSharma1201@users.noreply.github.com>
2024-01-13 12:28:12 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// data types summary //
+
+// primitive and non primitive //
+
+// 1 primitive //
+
+//string , number,boolean,null,undefined,symbol,BigInt
+
+ const score = 100
+console.log("score")
+console.log(typeof score);
+
+const isLoggedIn = false;
+//console.log("isLoggedIn");
+console.log(typeof isLoggedIn);
+ const outsideTemp = null;
+ console.log(typeof outsideTemp);
+ let useremail;
+ console.log(typeof useremail)
+const bigNumber = 123456n;
+console.log(typeof bigNumber);
+
+// non primitive //
+
+//Array
+//const heros = ["ABC","XYZ","EFG"];
+
+//objects
+
+//let myObj = {
+   // name:"hitesh",
+   // age:21,
+//}
+
+// function //
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof myFunction);
