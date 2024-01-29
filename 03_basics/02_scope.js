ecd546d4f0435931de4f1f487afdd33540a8d67a
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-01-29 20:35:01 +0530

    Update 02_scope.js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,9 +1,9 @@
 // var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // console.log("INNER: ", a);
     
 }
 
