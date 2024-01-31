856a0573f2378f8047528e0523f843e147eec6c7
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-01-31 18:44:02 +0530

    Update one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -39,11 +39,11 @@ for (let i = 1; i <= 10; i++) {
     
 // }
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//    console.log(`Value of i is ${index}`);
     
-}
+// }
