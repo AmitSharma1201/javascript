6d04f48a4db8e9c73ecb8296ff42c91272f284bb
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-02-04 21:08:22 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,13 +1,13 @@
-const user = {
-    username: "hitesh",
-    price: 999,
+// const user = {
+//     username: "hitesh",
+//     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
-    }
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
 
-}
+// }
 
 // user.welcomeMessage()
 // user.username = "sam"
