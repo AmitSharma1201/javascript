5bfe7632a4f99b6fb3e595bbc8097b1963d84aef
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-02-05 20:45:51 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,53 +1,53 @@
-//let score = "33" //
-//let score = null
-//let score = undefined
-console.log(typeof score);
-console.log(typeof(score));
+// //let score = "33" //
+// //let score = null
+// //let score = undefined
+// console.log(typeof score);
+// console.log(typeof(score));
 
-//let valueInNumber = Number(score);
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// //let valueInNumber = Number(score);
+// //console.log(typeof valueInNumber);
+// //console.log(valueInNumber);
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-//console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// //console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-//let stringNumber = Strin(someNumber);
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
+// //let stringNumber = Strin(someNumber);
+// //console.log(stringNumber);
+// //console.log(typeof stringNumber);
 
-//***************OPERATIONS**************//
+// //***************OPERATIONS**************//
 
-let value = 3
-let negValue = -value
-//console.log(negValue);
+// let value = 3
+// let negValue = -value
+// //console.log(negValue);
 
-//console.log(2+2);
-//console.log(2-2);
-//console.log(2*2);
-//console.log(2**2);
+// //console.log(2+2);
+// //console.log(2-2);
+// //console.log(2*2);
+// //console.log(2**2);
 
-let str1 = "hello"
-let str2 = " hitesh"
-let str3 = str1+str2;
-console.log(str3);
+// let str1 = "hello"
+// let str2 = " hitesh"
+// let str3 = str1+str2;
+// console.log(str3);
 
-//console.log("1"+2);
-//console.log(1+"2");
-//console.log("1"+2+2);
-//console.log(1+2+"2");
+// //console.log("1"+2);
+// //console.log(1+"2");
+// //console.log("1"+2+2);
+// //console.log(1+2+"2");
 
-//console.log(true);
-//console.log(+true);
-//console.log(1+"");
+// //console.log(true);
+// //console.log(+true);
+// //console.log(1+"");
 
-// let gameCounter = 100;
-// gameCounter++;
-// console.log(gameCounter);
+// // let gameCounter = 100;
+// // gameCounter++;
+// // console.log(gameCounter);
 
-console.log(1 + "33");
+// console.log(1 + "33");
 
 
 
