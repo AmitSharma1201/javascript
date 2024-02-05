fc4bfc5d7e6be5b84191393df69f9552decdb532
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-02-05 20:46:17 +0530

    Update 05_string.js

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,36 +1,36 @@
-const name = "hitesh"
-const repoCount = 50;
+// const name = "hitesh"
+// const repoCount = 50;
 
-//console.log(name + repoCount + " value");
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// //console.log(name + repoCount + " value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-//const gameName = new String('hiteshhc')
-//console.log(gameName[0]);
-//console.log(gameName.__proto__);
+// //const gameName = new String('hiteshhc')
+// //console.log(gameName[0]);
+// //console.log(gameName.__proto__);
 
-//console.log(gameName.length)
-//console.log(gameName.toUpperCase());
-//console.log(gameName.charAt(2));
-//console.log(gameName.indexOf('t'));
+// //console.log(gameName.length)
+// //console.log(gameName.toUpperCase());
+// //console.log(gameName.charAt(2));
+// //console.log(gameName.indexOf('t'));
 
-const gameName = new String('hitesh-hc-com')
+// const gameName = new String('hitesh-hc-com')
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
 
-const newStringOne = "  hitesh  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "  hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const anotherString = gameName.slice(-8,4);
-console.log(anotherString);
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
 
-//***********************commit************//
-//const url = "https://hitesh.com/hitesh%20choudhary";
-//console.log(url.replace('%20','-'));
-//console.log(url.includes('hitesh'));
+// //***********************commit************//
+// //const url = "https://hitesh.com/hitesh%20choudhary";
+// //console.log(url.replace('%20','-'));
+// //console.log(url.includes('hitesh'));
 
-//console.log(gameName.split('-'));
+// //console.log(gameName.split('-'));
 
 
