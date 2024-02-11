429f4d1037e4c30eb8061bb41f378e4769413e5f
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-02-11 22:13:46 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,23 +1,23 @@
-"use strict"; //treat all js code as newer version
-// alert (3+3) // we are using nodejs,not browser
- console.log(3+3)
- console.log("hitesh")  // code readibility should me high//
+// "use strict"; //treat all js code as newer version
+// // alert (3+3) // we are using nodejs,not browser
+//  console.log(3+3)
+//  console.log("hitesh")  // code readibility should me high//
 
- //  primitive datatypes //
+//  //  primitive datatypes //
 
- // number => 2 to power 53(range)
- // bignit used for large numbers eg stockmarket,trading etc
- // string =>""
- // boolean => true/false
- // null(representation of empty value ) => standalone value
- // undefined => value doesnot defined 
- //symbol => unique
+//  // number => 2 to power 53(range)
+//  // bignit used for large numbers eg stockmarket,trading etc
+//  // string =>""
+//  // boolean => true/false
+//  // null(representation of empty value ) => standalone value
+//  // undefined => value doesnot defined 
+//  //symbol => unique
 
- // objects //
- console.log(typeof age);
- console.log(typeof null);
- //null = it is a object
- console.log(typeof undefined);
+//  // objects //
+//  console.log(typeof age);
+//  console.log(typeof null);
+//  //null = it is a object
+//  console.log(typeof undefined);
 
 
 
