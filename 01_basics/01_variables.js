1bbbf7a18f12c626dcdf108848c37a86ed471712
Amit Sharma <155089920+AmitSharma1201@users.noreply.github.com>
2024-03-20 21:19:54 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
-// const accountId = 144553
-// let accountEmail = "hitesh@googlrr.com"
-// var accountPassword = "12345"
-// accountCity = "jaipur"
-// let accountState;
+const accountId = 144553
+let accountEmail = "hitesh@googlrr.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
 
-// //accountId = 2 // not allowed 
+//accountId = 2 // not allowed 
 
-// accountEmail = "abc@gmail.com"
-// accountPassword = "asdf"
-// accountCity = "banglore"
-// console.log(accountId)
-// console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
+accountEmail = "abc@gmail.com"
+accountPassword = "asdf"
+accountCity = "banglore"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
 
